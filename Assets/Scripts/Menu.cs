632398a1f50b21cac005a726b6c5666ba36171d0
Vladislav Igorevich <vladislav.igorevich.workmail@gmail.com>
2022-05-14 15:58:45 +0300

    Added menu, timer

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Menu : MonoBehaviour
+{
+    private List<GameObject> buttons = new List<GameObject>();
+
+    private void Awake()
+    {
+        foreach (Transform tr in transform)
+        {
+            buttons.Add(tr.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowOrHideMenu();
+        }
+    }
+
+    public void ShowOrHideMenu()
+    {
+        PauseOrPlay();
+
+        foreach (var button in buttons)
+        {
+            button.SetActive(!button.activeSelf);
+        }
+    }
+
+    private void PauseOrPlay()
+    {
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    readonly private int levelTime = 60;
+
+    private Text timerText;
+
+    private int currentTime;
+
+    void Start()
+    {
+        timerText = GetComponent<Text>();
+
+        currentTime = levelTime;
+        timerText.text = currentTime.ToString();
+        StartTimer();
+    }
+
+    public void StartTimer()
+    {
+        StartCoroutine(DecreaseTimer());
+    }
+
+    private IEnumerator DecreaseTimer(int decrement = 1)
+    {
+        while (currentTime > 0)
+        {
+            yield return new WaitForSeconds(1);
+
+            currentTime -= decrement;
+            timerText.text = currentTime.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
