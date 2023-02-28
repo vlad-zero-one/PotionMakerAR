5b60043f334fedcd76ab19e48de0dddb69828388
Vladislav Igorevich <vladislav.igorevich.workmail@gmail.com>
2023-02-28 22:37:30 +0300

    Fix unfocusing camera, change window orientation

---
+++ Assets/Scripts/Utils/CameraAutofocus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class CameraAutofocus : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        yield return new WaitUntil(() => VuforiaARController.Instance.HasStarted);
+
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+}
